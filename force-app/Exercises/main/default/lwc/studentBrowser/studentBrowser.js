9668e94096b725c2c96c21def3594880d6f0fa6a
Himanshuatstratos <himanshu.joshi@stratos.studio>
2022-04-20 09:53:46 +0530

    Exercise 3-1 Created Wrapper Component and Action

--- force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
+++ force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
@@ -1,17 +1,41 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from "lwc";
+import getStudents from "@salesforce/apex/StudentBrowser.getStudents";
+import { publish, MessageContext } from "lightning/messageService";
+import SELECTED_STUDENT_CHANNEL from "@salesforce/messageChannel/SelectedStudentChannel__c";
 
 export default class StudentBrowser extends LightningElement {
-    studentList = [];
-    constructor() {
-        super() ;
-        const studentNames = ['Rad', 'Stuart', 'Andres', 'Rahul', 'Amit', 'Simon'];
-        this.studentList = studentNames.map( (studentName, index)=> {
-            return {
-            'sobjectType': 'Contact',
-            'Name': studentName,
-            'PhotoUrl': '/services/images/photo/003B0FakePictId',
-            'Id': index
-            };
-        });
-        }
+	selectedDeliveryId = "";
+	selectedInstructorId = "";
+	studentList = [];
+
+	@wire(getStudents, { instructorId: "$selectedInstructorId", courseDeliveryId: "$selectedDeliveryId" }) students;
+	@wire(MessageContext) messageContext;
+
+	constructor() {
+		super();
+		const studentNames = ["Rad", "Stuart", "Andres", "Rahul", "Amit", "Simon"];
+		this.studentList = studentNames.map((studentName, index) => {
+			return {
+				sobjectType: "Contact",
+				Name: studentName,
+				PhotoUrl: "/services/images/photo/003B0FakePictId",
+				Id: index
+			};
+		});
+	}
+
+	handleFilterChange(event) {
+		console.log("Inside handleFilterChange ");
+		this.selectedDeliveryId = event.detail.deliveryId;
+		this.selectedInstructorId = event.detail.instructorId;
+	}
+
+	handleStudentSelected(event) {
+		const studentId = event.detail.studentId;
+		this.updateSelectedStudent(studentId);
+	}
+
+	updateSelectedStudent(studentId) {
+		publish(this.messageContext, SELECTED_STUDENT_CHANNEL, {studentId: studentId});
+	}
 }
--- force-app/Exercises/main/default/lwc/studentTile/studentTile.js
+++ force-app/Exercises/main/default/lwc/studentTile/studentTile.js
@@ -7,14 +7,15 @@ export default class StudentTile extends LightningElement {
         Name: '[Himanshu Joshi]',
         PhotoUrl: '/services/images/photo/003B0FakePictId',
         };
-
+    @api selectedStudentId = '';
     get tileSelected() 
     {
-        return this.isSelected ? "tile selected" : "tile";
+        return (this.selectedStudentId===this.student.Id) ? "tile selected" : "tile";   
     }
 
     studentClick(){
-        alert(this.student.Name);
+        const evt = new CustomEvent('studentselected', { bubbles: true, composed: true, detail: { studentId: this.student.Id }});
+        this.dispatchEvent(evt) ;
     }
 
 }
